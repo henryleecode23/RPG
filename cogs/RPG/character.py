dcf8ca4271eba2936cb4809d2976a21d1907cca6
henryleecode23 <99625462+henryleecode23@users.noreply.github.com>
2023-05-01 15:45:34 +0800

    style: embed field inline

--- cogs/RPG/character.py
+++ cogs/RPG/character.py
@@ -16,10 +16,15 @@ class CharacterCog(CogBase):
             return
         character = Character.random_create(id=ctx.author.id, name=ctx.author.name)
         embed = discord.Embed(title="Here is your character", color=0x57e389)
-        embed.add_field(name="HP", value=f"{character.HP}/{character.MAX_HP}", inline=False)
-        embed.add_field(name="MP", value=f"{character.MP}/{character.MAX_MP}", inline=False)
+        embed.add_field(name="HP", value=f"{character.HP}/{character.MAX_HP}", inline=True)
+        embed.add_field(name="MP", value=f"{character.MP}/{character.MAX_MP}", inline=True)
         embed.add_field(name="屬性", value=
-                        f"STR: {character.STR}\nDEX: {character.DEX}\nCON: {character.CON}\nINT: {character.INT}\nWIS: {character.WIS}\nCHA: {character.CHA}",
+                        f"STR: {character.STR}\n \
+                        DEX: {character.DEX}\n \
+                        CON: {character.CON}\n \
+                        INT: {character.INT}\n \
+                        WIS: {character.WIS}\n \
+                        CHA: {character.CHA}",
                         inline=False)
         embed.set_thumbnail(url=ctx.author.display_avatar.url)
         await ctx.respond(embed=embed, ephemeral=True)
@@ -33,9 +38,16 @@ class CharacterCog(CogBase):
             return
         character = Character.create_from_dict(data)
         embed=discord.Embed(title=ctx.author.name, description=ctx.author.mention, color=0x57e389)
-        embed.add_field(name="HP", value=f"{character.HP}/{character.MAX_HP}", inline=False)
-        embed.add_field(name="MP", value=f"{character.MP}/{character.MAX_MP}", inline=False)
-        embed.add_field(name="屬性", value=f"STR: {character.STR}\nDEX: {character.DEX}\nCON: {character.CON}\nINT: {character.INT}\nWIS: {character.WIS}\nCHA: {character.CHA}", inline=False)
+        embed.add_field(name="HP", value=f"{character.HP}/{character.MAX_HP}", inline=True)
+        embed.add_field(name="MP", value=f"{character.MP}/{character.MAX_MP}", inline=True)
+        embed.add_field(name="屬性", value=
+                        f"STR: {character.STR}\n \
+                        DEX: {character.DEX}\n \
+                        CON: {character.CON}\n \
+                        INT: {character.INT}\n \
+                        WIS: {character.WIS}\n \
+                        CHA: {character.CHA}",
+                        inline=False)
         embed.set_thumbnail(url=ctx.author.display_avatar.url)
         await ctx.respond(embed=embed, ephemeral=True)
 
@@ -55,9 +67,16 @@ class CharacterCog(CogBase):
             return
         character = Character.create_from_dict(data)
         embed=discord.Embed(title=ctx.author.name, description=ctx.author.mention, color=0x57e389)
-        embed.add_field(name="HP", value=f"{character.HP}/{character.MAX_HP}", inline=False)
-        embed.add_field(name="MP", value=f"{character.MP}/{character.MAX_MP}", inline=False)
-        embed.add_field(name="屬性", value=f"STR: {character.STR}\nDEX: {character.DEX}\nCON: {character.CON}\nINT: {character.INT}\nWIS: {character.WIS}\nCHA: {character.CHA}", inline=False)
+        embed.add_field(name="HP", value=f"{character.HP}/{character.MAX_HP}", inline=True)
+        embed.add_field(name="MP", value=f"{character.MP}/{character.MAX_MP}", inline=True)
+        embed.add_field(name="屬性", value=
+                        f"STR: {character.STR}\n \
+                        DEX: {character.DEX}\n \
+                        CON: {character.CON}\n \
+                        INT: {character.INT}\n \
+                        WIS: {character.WIS}\n \
+                        CHA: {character.CHA}",
+                        inline=False)
         embed.set_thumbnail(url=ctx.author.display_avatar.url)
         await ctx.respond(embed=embed)
         
